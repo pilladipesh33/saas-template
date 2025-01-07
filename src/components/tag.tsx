b87054a69d6abeec24af5f88a212b609ec888de6
pilladipesh33 <pilladipesh.pd@gmail.com>
2025-01-07 23:55:20 +0530

    intro section added

---
+++ src/components/tag.tsx
@@ -0,0 +1,19 @@
+"use-client";
+
+import { cn } from "@/lib/utils";
+import { HTMLAttributes } from "react";
+
+export const Tag = (props: HTMLAttributes<HTMLDivElement>) => {
+  return (
+    <div
+      className={cn(
+        "inline-flex border border-primary gap-2 text-primary px-3 py-1 rounded-full uppercase items-center",
+        props.className,
+      )}
+      {...props}
+    >
+      <span>&#10038;</span>
+      <span className="text-sm">{props.children}</span>
+    </div>
+  );
+};
--- src/sections/Introduction.tsx
+++ src/sections/Introduction.tsx
@@ -1,5 +1,22 @@
+import { Tag } from "@/components/tag";
+
 const text = `You're racing to create exceptional work, but traditional design tools slow you down with unnecessary complexity and steep learning curves.`;
 
 export default function Introduction() {
-    return <div>Introduction</div>;
+  return (
+    <section className="py-28 lg:py-40">
+      <div className="container">
+        <div className="flex justify-center">
+          <Tag>Introducing Layers</Tag>
+        </div>
+        <div className="text-4xl md:text-6xl lg:text-7xl font-medium text-center mt-10">
+          <span>Your creative process deserves better.</span>{" "}
+          <span className="text-accent/15">{text}</span>
+          <span className="text-primary block">
+            That&apos;s why we built Layers.
+          </span>
+        </div>
+      </div>
+    </section>
+  );
 }
