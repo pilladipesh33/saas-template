504d1639e2c000c7fd51fea76f3cba9cdd9c7388
Dipesh <99866329+pilladipesh33@users.noreply.github.com>
2025-01-23 15:51:19 +0530

    CTA section animation added

--- src/sections/CallToAction.tsx
+++ src/sections/CallToAction.tsx
@@ -1,16 +1,47 @@
+"use client";
+
+import { AnimationPlaybackControls, motion, useAnimate } from "motion/react";
+import { useEffect, useRef, useState } from "react";
+
 export default function CallToAction() {
-  return (
-    <section className="py-24">
-      <div className="overflow-x-clip p-4 flex ">
-        <div className="flex flex-none gap-16 text-7xl md:text-8xl  font-medium">
-          {Array.from({ length: 10 }).map((_, i) => (
-            <div key={i} className="flex items-center gap-16">
-              <span className="text-primary text-7xl">&#10038;</span>
-              <span>Try it for free</span>
-            </div>
-          ))}
-        </div>
-      </div>
-    </section>
-  );
+	const [isHovered, setIsHovered] = useState(false);
+	const animationRef = useRef<AnimationPlaybackControls>(null);
+	const [scope, animate] = useAnimate();
+
+	useEffect(() => {
+		animationRef.current = animate(
+			scope.current,
+			{ x: "-50%" },
+			{ duration: 30, repeat: Infinity, ease: "linear" }
+		);
+	}, []);
+
+	useEffect(() => {
+		if (animationRef.current) {
+			if (isHovered) {
+				animationRef.current.speed = 0.5;
+			} else {
+				animationRef.current.speed = 1;
+			}
+		}
+	}, [isHovered]);
+	return (
+		<section className="py-24">
+			<div className="overflow-x-clip p-4 flex ">
+				<motion.div
+					ref={scope}
+					className="flex flex-none gap-16 pr-16 text-7xl md:text-8xl group font-medium cursor-pointer"
+					onMouseEnter={() => setIsHovered(true)}
+					onMouseLeave={() => setIsHovered(false)}
+				>
+					{Array.from({ length: 10 }).map((_, i) => (
+						<div key={i} className="flex items-center gap-16">
+							<span className="text-primary text-7xl">&#10038;</span>
+							<span className="group-hover:text-primary">Try it for free</span>
+						</div>
+					))}
+				</motion.div>
+			</div>
+		</section>
+	);
 }
